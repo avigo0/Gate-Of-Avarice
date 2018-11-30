0b6d567a9c46bcc3317e4481301f80ca474fc285
Peter Nguyen <37283965+phqn@users.noreply.github.com>
2018-11-29 16:54:43 -0800

    Create follow_camera.cs

---
+++ follow_camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_camera : MonoBehaviour {
+    public Transform targetTransform;
+    Vector3 tempVec3 = new Vector3();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate() {
+        tempVec3.x = targetTransform.position.x;
+        tempVec3.y = this.transform.position.y;
+        tempVec3.z = this.transform.position.z;
+        this.transform.position = tempVec3;
+    }
+}
